0b30965846d66146d601294c5503280a0095050f
ssi218 <ssingh217@sapient.com>
2017-03-24 00:23:49 +0530

    spring learn base

---
+++ demo/src/main/java/demo/DemoApplication.java
@@ -0,0 +1,95 @@
+package demo;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DemoApplication {
+
+	@Bean
+	CommandLineRunner runner(ReservationRepository rr){
+		return args -> {
+			Arrays.asList("Shailendra,Ranjeet,Vipul,Purti".split(","))
+			.forEach(x ->rr.save(new Reservation(x)));
+			rr.findAll().forEach(System.out::println);
+		};
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController{
+	
+	@Value("${message:Hello default}")
+	private String message;
+	
+	@RequestMapping("/message")
+	String message(){
+		return this.message;
+	}
+}
+
+@RepositoryRestResource
+interface ReservationRepository extends JpaRepository<Reservation, Long>{
+	@RestResource(path = "by-name")
+	Collection<Reservation> findByReservationName(@Param("rn")String rn);
+}
+
+@Entity
+class Reservation{
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String reservationName;
+	
+	public Reservation() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Reservation(String reservationName) {
+		super();
+		this.reservationName = reservationName;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getReservationName() {
+		return reservationName;
+	}
+	public void setReservationName(String reservationName) {
+		this.reservationName = reservationName;
+	}
+	@Override
+	public String toString() {
+		return "Reservation [id=" + id + ", reservationName=" + reservationName + "]";
+	}
+	
+	
+	
+}
